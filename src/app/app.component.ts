373fd0e44e4a11ce02c32e03daf3c7cda680821a
Aleksandra Milivojevic <aleksandra.milivojevic.94@gmail.com>
2023-01-03 22:08:17 +0100

    Initialize alphabets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { cyrilicAlphabet, latinAlphabet } from './alphabet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slovoc';
+  cyrilicAlphabet = cyrilicAlphabet;
+  latinAlphabet = latinAlphabet;
 }
