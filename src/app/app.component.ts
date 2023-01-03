6e642695cc379826b81b098b2751dc395b770fc2
Aleksandra Milivojevic <aleksandra.milivojevic.94@gmail.com>
2023-01-03 23:01:31 +0100

    Letter selection done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,22 @@ export class AppComponent {
   title = 'slovoc';
   cyrilicAlphabet = cyrilicAlphabet;
   latinAlphabet = latinAlphabet;
+  selectedCyrilicIndex: number = -1;
+  selectedLatinIndex: number = -1;
+
+  selectCyrilic(index: number) {
+    if (this.selectedCyrilicIndex == index) {
+      this.selectedCyrilicIndex = -1;
+    } else {
+      this.selectedCyrilicIndex = index;
+    }
+  }
+
+  selectLatin(index: number) {
+    if (this.selectedLatinIndex == index) {
+      this.selectedLatinIndex = -1;
+    } else {
+      this.selectedLatinIndex = index;
+    }
+  }
 }
