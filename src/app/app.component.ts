ce9a0e74c8afa6b55904409db89b3b88f53ef192
Aleksandra Milivojevic <aleksandra.milivojevic.94@gmail.com>
2023-01-03 23:19:08 +0100

    Exemple words added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { cyrilicAlphabet, latinAlphabet } from './alphabet';
+import { cyrilicWords, latinWords } from './words';
+
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,8 @@ export class AppComponent {
   title = 'slovoc';
   cyrilicAlphabet = cyrilicAlphabet;
   latinAlphabet = latinAlphabet;
+  cyrilicWords = cyrilicWords;
+  latinWords = latinWords;
   selectedCyrilicIndex: number = -1;
   selectedLatinIndex: number = -1;
 
